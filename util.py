fa019723ff6f0566b570a3a24a0d1a65128ec0bb
Dominic Jack <thedomjack@gmail.com>
2018-11-19 18:05:23 +1000

    input from six

--- util.py
+++ util.py
@@ -1,16 +1,12 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-try:
-    get_input = raw_input
-except NameError:
-    get_input = input
+import six
 
 
 def maybe_stop():
     """Prompt user to continue or stop."""
-    inp = get_input('Continue? (Y/n): ').lower().rstrip()
+    inp = six.moves.input('Continue? (Y/n): ').lower().rstrip()
     if inp == 'n':
         # exit()
         raise Exception('Stopped')
